12ad4cc478b0b3b0d940ef3d97b699fc8294f567
Wai-Shing Luk <luk036@gmail.com>
2025-03-26 16:14:19 +0800

    cleanup xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,38 +1,35 @@
 add_rules("mode.debug", "mode.release", "mode.coverage")
-add_requires("fmt", {alias = "fmt"})
-add_requires("doctest", {alias = "doctest"})
--- add_requires("boost", {configs = {context = true, container = true}})
--- add_requires("microsoft-gsl", {alias = "msgsl"})
--- add_requires("conan::range-v3/0.11.0", {alias = "range-v3"})
+add_requires("fmt", { alias = "fmt" })
+add_requires("doctest", { alias = "doctest" })
 
 set_languages("c++23")
 
 if is_plat("linux") then
     set_warnings("all", "error")
-    add_cxflags("-Wconversion", {force = true})
+    add_cxflags("-Wconversion", { force = true })
 elseif is_plat("windows") then
-    add_cxflags("/W4 /WX /wd4127 /wd4996", {force = true})
+    add_cxflags("/W4 /WX /wd4127 /wd4996", { force = true })
 end
 
 if is_mode("coverage") then
-    add_cxflags("-ftest-coverage", "-fprofile-arcs", {force = true})
+    add_cxflags("-ftest-coverage", "-fprofile-arcs", { force = true })
 end
 
 target("NetlistX")
-    set_kind("static")
-    add_includedirs("include", {public = true})
-    add_includedirs("../py2cpp/include", {public = true})
-    add_includedirs("../xnetwork-cpp/include", {public = true})
-    add_files("source/*.cpp")
+set_kind("static")
+add_includedirs("include", { public = true })
+add_includedirs("../py2cpp/include", { public = true })
+add_includedirs("../xnetwork-cpp/include", { public = true })
+add_files("source/*.cpp")
 
 target("test_netlistx")
-    set_kind("binary")
-    add_deps("NetlistX")
-    add_includedirs("include", {public = true})
-    add_includedirs("../py2cpp/include", {public = true})
-    add_includedirs("../xnetwork-cpp/include", {public = true})
-    add_files("test/source/*.cpp")
-    add_packages("fmt", "doctest")
+set_kind("binary")
+add_deps("NetlistX")
+add_includedirs("include", { public = true })
+add_includedirs("../py2cpp/include", { public = true })
+add_includedirs("../xnetwork-cpp/include", { public = true })
+add_files("test/source/*.cpp")
+add_packages("fmt", "doctest")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
@@ -102,4 +99,3 @@ target("test_netlistx")
 --
 -- @endcode
 --
-

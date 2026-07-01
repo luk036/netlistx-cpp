c305ab907ae154e6e9f8293db899e2cb26c18582
Wai-Shing Luk <luk036@gmail.com>
2026-07-01 13:53:13 +0800

    fix: guard rapidcheck link with os.isfile() to fix macOS build

--- xmake.lua
+++ xmake.lua
@@ -76,26 +76,32 @@ target("test_netlistx")
 	end
 	add_tests("default")
 
--- Check if rapidcheck was downloaded by CMake (check both build and build_test directories)
+-- Check if rapidcheck was built by CMake (check both build and build_test directories)
 local build_dirs = { "build", "build_test" }
 local rapidcheck_dir = nil
 local rapidcheck_lib_dir = nil
+local rapidcheck_lib = nil
 
 for _, build_dir in ipairs(build_dirs) do
 	local candidate_src = path.join(os.projectdir(), build_dir, "_deps", "rapidcheck-src")
-	local candidate_lib = path.join(os.projectdir(), build_dir, "_deps", "rapidcheck-build")
+	local candidate_lib_dir = path.join(os.projectdir(), build_dir, "_deps", "rapidcheck-build")
+	local candidate_lib = nil
 	if is_plat("windows") then
-		candidate_lib = path.join(candidate_lib, "Release")
+		candidate_lib_dir = path.join(candidate_lib_dir, "Release")
+		candidate_lib = path.join(candidate_lib_dir, "rapidcheck.lib")
+	else
+		candidate_lib = path.join(candidate_lib_dir, "librapidcheck.a")
 	end
 
-	if os.isdir(candidate_src) and os.isdir(candidate_lib) then
+	if os.isdir(candidate_src) and os.isfile(candidate_lib) then
 		rapidcheck_dir = candidate_src
-		rapidcheck_lib_dir = candidate_lib
+		rapidcheck_lib_dir = candidate_lib_dir
+		rapidcheck_lib = candidate_lib
 		break
 	end
 end
 
-if rapidcheck_dir and rapidcheck_lib_dir then
+if rapidcheck_dir and rapidcheck_lib then
 	add_includedirs(path.join(rapidcheck_dir, "include"))
 	add_linkdirs(rapidcheck_lib_dir)
 	add_links("rapidcheck")
